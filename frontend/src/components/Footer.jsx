19b30a98db714db4afdc778a80864b24feda9d45
Paulo Guilherme <92047054+PauloGDev@users.noreply.github.com>
2024-12-19 12:16:11 -0300

    First Commit
    Adicionando funções já criadas anteriormente

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+        <div>
+          <img src={assets.logo} className='mb-5 w-32' alt="" />
+          <p className='w-full md:w-2:3 text-gray-600'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+        </div>
+
+        <div>
+          <p className='text-x1 font-medium mb-5'>COMPANY</p>
+          <ul className='flex flex-col gap-1 text-gray-600'>
+          <li>Home</li>
+          <li>About</li>
+          <li>Delivery</li>
+          <li>Privacy Policy</li>
+          </ul>
+        </div>
+
+        <div>
+          <p className='text-x1 font-medium mb-5'>GET IN TOUCH</p>
+          <ul className='flex flex-col gap-1 text-gray-600'>
+          <li>+55 (99) 9 9999-9999</li>
+          <li>Contact@contact.com</li>
+          </ul>
+        </div>
+
+      </div>
+
+      <div>
+        <hr />
+        <p className='py-5 text-sm text-center'>Copyright 2025@ Paulo Oliveira - All Right Reserved.</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default Footer
